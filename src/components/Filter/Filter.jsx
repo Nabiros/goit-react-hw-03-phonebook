7e6d29c771917fed74495d0757d66d300ce7cf08
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-09-23 22:30:10 +0300

    fix filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,29 +1,27 @@
-import { Component } from "react";
+import React from 'react';
 import { Wrapper, Label, Input } from "./Filter.styled";
 import PropTypes from 'prop-types';
 
-export class Filter extends Component {
+    
+export const Filter = ({getFilterName, value}) => {
     getFilterName = (e) => {
         const { onChange } = this.props;
 
         return onChange(e.currentTarget.value);
-    };
+};
 
-    render() {
-        const { value } = this.props;
-        return (
-            <Wrapper>
+    return (
+        <Wrapper>
                 <Label htmlFor="filter">Find contacts by name</Label>
                 <Input
                     type="text"
                     name="filter"
                     value={value}
-                    onChange={this.getFilterName}
+                    onChange={getFilterName}
                 />
             </Wrapper>
-        );
-    };
-};
+    )
+}
 
 Filter.propTypes = {
     onChange: PropTypes.func,
